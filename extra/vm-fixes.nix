02fc262116a6fef36962f028f3d748f9bf996be8
jwren0 <jwren0@protonmail.com>
2023-07-24 00:42:56 +0100

    Added extra vm fixes

--- extra/vm-fixes.nix
+++ extra/vm-fixes.nix
@@ -2,4 +2,9 @@
     services.xserver.displayManager.setupCommands = ''
     ${pkgs.xorg.xrandr}/bin/xrandr -s 1920x1080
     '';
+    boot.kernelParams = [
+        "video=1920x1080@60"
+    ];
+    boot.loader.grub.gfxmodeBios = "1920x1080";
+    boot.loader.grub.gfxmodeEfi = "1920x1080";
 }
