517df07ac6ebb6aed594a1e43914fc636b825c0f
jwren0 <jwren0@protonmail.com>
2023-07-28 01:00:46 +0100

    Migrated zsh to home-manager

---
+++ home-manager/zsh.nix
@@ -0,0 +1,111 @@
+{ pkgs, ... } : {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    autocd = true;
+    defaultKeymap = "emacs";
+    dotDir = ".config/zsh";
+
+    history.path = "$ZDOTDIR/zsh_history";
+
+    initExtra = ''
+    setopt nomatch
+    setopt prompt_subst
+
+    zstyle ":vcs_info:*" check-for-changes true
+    zstyle ":vcs_info:*" get-unapplied true
+    zstyle ":vcs_info:*" unstagedstr " %F{yellow}+%F{green}"
+    zstyle ":vcs_info:*" stagedstr " %F{yellow}+%F{green}"
+    zstyle ":vcs_info:git:*" formats " %F{green}(%b%u%c)%f"
+    zstyle ":vcs_info:git:*" actionformats " %F{green}(%b|%a%u%c)%f"
+
+    autoload -Uz add-zsh-hook vcs_info
+    add-zsh-hook precmd vcs_info
+
+    PROMPT="%n%F{green}@%m%f\$vcs_info_msg_0 %1~%F{green}>%f "
+
+    bindkey "\e[3~" delete-char
+    bindkey "^[[1;3C" forward-word
+    bindkey "^[[1;3D" backward-word
+    bindkey "^[[1;5C" forward-word
+    bindkey "^[[1;5D" backward-word
+
+    poweroff() {
+        printf "Really poweroff? [y/N] "
+        read opt
+
+        if [ "$opt" = "y" ] || [ "$opt" = "Y" ]; then
+            printf "Powering off...\n"
+            sudo poweroff
+        else
+            printf "Poweroff cancelled\n"
+        fi
+    }
+
+    reboot() {
+        printf "Really reboot? [y/N] "
+        read opt
+
+        if [ "$opt" = "y" ] || [ "$opt" = "Y" ]; then
+            printf "Rebooting...\n"
+            sudo reboot
+        else
+            printf "Reboot cancelled\n"
+        fi
+    }
+
+    rm() {
+        for _f in $@; do
+            ${pkgs.trash-cli}/bin/trash $_f
+        done
+
+        unset _f
+    }
+    '';
+
+    sessionVariables = let
+        less = "${pkgs.less}/bin/less";
+        nvim = "${pkgs.neovim}/bin/nvim";
+    in {
+        PAGER = less;
+        EDITOR = nvim;
+        VISUAL = nvim;
+        WORDCHARS = "$.";
+        LESSHISTFILE = "/dev/null";
+        LESS_TERMCAP_md = "$(printf \"\\e[1;34m\")";
+        LESS_TERMCAP_me = "$(printf \"\\e[0m\")";
+        LESS_TERMCAP_us = "$(printf \"\\e[1;4;36m\")";
+        LESS_TERMCAP_ue = "$(printf \"\\e[0m\")";
+        CARGO_HOME = "$HOME/.local/share/cargo";
+        RUSTUP_HOME = "$HOME/.local/share/rustup";
+    };
+
+    shellAliases = {
+        "ls" = "exa -Fg --icons --group-directories-first";
+        "la" = "ls -la";
+        "ll" = "ls -l";
+        "al" = "la";
+        "sl" = "ls";
+        "ip" = "ip -c";
+        "bat" = "bat --theme OneHalfDark";
+        "cal" = "cal -m";
+        "cd.." = "cd ..";
+        "diff" = "diff -u --color=auto";
+        "less" = "less -RF";
+        "open" = "mimeopen";
+        "pman" = "MANPAGER=zathura man -T pdf";
+        "tree" = "tree -CF --dirsfirst";
+        "update" = "sudo nix-channel --update && sudo nixos-rebuild switch";
+        "shutdown" = "poweroff";
+
+        # Directory aliases
+        "gdir" = "cd $HOME/Documents/Programming/Git";
+        "ldir" = "cd $HOME/Documents/Programming/Local";
+        "pdir" = "cd $HOME/Documents/Programming";
+
+        # Safer aliases
+        "cp" = "cp -i";
+        "ln" = "ln -i";
+        "mv" = "mv -i";
+    };
+}
