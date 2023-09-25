1c1e8a7889ff9621c3f4143136aa39cce3712857
jwren0 <jwren0@protonmail.com>
2023-09-25 02:38:24 +0100

    Improve gpg config

--- home-manager/gpg.nix
+++ home-manager/gpg.nix
@@ -1,4 +1,50 @@
 user: {
     enable = true;
     homedir = "${user.home}/.local/share/gnupg";
+    settings = {
+        no-greeting = true;
+        photo-viewer = "sxiv %i";
+
+        armor = true;
+        expert = true;
+        require-secmem = true;
+        min-rsa-length = "4096";
+
+        personal-cipher-preferences = [
+            "AES256"
+            "AES192"
+            "AES128"
+            "TWOFISH"
+        ];
+        personal-digest-preferences = [
+            "SHA512"
+            "SHA384"
+            "SHA256"
+            "SHA224"
+            "RIPEMD160"
+        ];
+        personal-compress-preferences = [
+            "BZIP2"
+            "ZIP"
+            "Uncompressed"
+        ];
+
+        s2k-cipher-algo = "AES256";
+        s2k-digest-algo = "SHA512";
+
+        disable-cipher-algo = [
+            "IDEA"
+            "3DES"
+            "BLOWFISH"
+        ];
+
+        disable-pubkey-algo = [
+            "ELG"
+            "DSA"
+        ];
+
+        weak-digest = [
+            "SHA1"
+        ];
+    };
 }
